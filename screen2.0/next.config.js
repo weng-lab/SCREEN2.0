d098420d7e88244b9475bbfa9360ef28d1c625fd
Jonathan Fisher <jpfisher72@gmail.com>
2023-05-25 11:01:54 -0400

    New Workflow, next config changes

--- screen2.0/next.config.js
+++ screen2.0/next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+// Configure the Next.js next/image, next/link, and next/router to expect the github pages URL
+// Note: High probability this may have implications for the real deployment, I'm not completely sure
+// https://nextjs.org/docs/pages/api-reference/next-config-js/assetPrefix
+// https://nextjs.org/docs/pages/api-reference/next-config-js/basePath
+
+// May need to mess with images 
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
@@ -7,6 +28,8 @@ const nextConfig = {
   },
   generateEtags: false,
   trailingSlash: false,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 }
 
 module.exports = nextConfig
