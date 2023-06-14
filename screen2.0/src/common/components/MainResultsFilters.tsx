a368c25fa7d578d55cacc4894b19e4b94875be44
Jonathan Fisher <jpfisher72@gmail.com>
2023-06-14 14:16:07 -0400

    Chromatin Filters working

--- screen2.0/src/common/components/MainResultsFilters.tsx
+++ screen2.0/src/common/components/MainResultsFilters.tsx
@@ -6,12 +6,30 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 // import { Paper } from '@mui/material';
 
-// import { RangeSlider } from '@weng-lab/psychscreen-ui-components';
+import Link from 'next/link';
 
-export default function MainResultsFilters() {
-  const [DNaseRange, setDNaseRange] = React.useState([1.64, 10])
+import { RangeSlider } from '@weng-lab/psychscreen-ui-components';
+
+//Need to go back and define the types in mainQueryParams object
+export default function MainResultsFilters(props: { mainQueryParams: any }) {
+  //No alternatives provided for default, as all these attributes should exist and are given a default value in Search's page.tsx
+  const [DNaseStart, setDNaseStart] = React.useState(props.mainQueryParams.dnase_s)
+  const [DNaseEnd, setDNaseEnd] = React.useState(props.mainQueryParams.dnase_e)
+  const [H3K4me3Start, setH3K4me3Start] = React.useState(props.mainQueryParams.h3k4me3_s)
+  const [H3K4me3End, setH3K4me3End] = React.useState(props.mainQueryParams.h3k4me3_e)
+  const [H3K27acStart, setH3K27acStart] = React.useState(props.mainQueryParams.h3k27ac_s)
+  const [H3K27acEnd, setH3K27acEnd] = React.useState(props.mainQueryParams.h3k27ac_e)
+  const [CTCFStart, setCTCFStart] = React.useState(props.mainQueryParams.ctcf_s)
+  const [CTCFEnd, setCTCFEnd] = React.useState(props.mainQueryParams.ctcf_e)
+
+  const constructURL = () => {
+    const url = `search?assembly=${props.mainQueryParams.assembly}&chromosome=${props.mainQueryParams.chromosome}&start=${props.mainQueryParams.start}&end=${props.mainQueryParams.end}&dnase_s=${DNaseStart}&dnase_e=${DNaseEnd}&h3k4me3_s=${H3K4me3Start}&h3k4me3_e=${H3K4me3End}&h3k27ac_s=${H3K27acStart}&h3k27ac_e=${H3K27acEnd}&ctcf_s=${CTCFStart}&ctcf_e=${CTCFEnd}`
+    return url
+  }
 
   return (
     <>
@@ -39,11 +57,59 @@ export default function MainResultsFilters() {
           <Typography>Chromatin Signals</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          {/* <RangeSlider title='DNase Z-Score' width={250} defaultStart={1.64} defaultEnd={10} min={-10} max={10} minDistance={1} step={0.01} onChange={(value) => setDNaseRange(value)}/> */}
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+          <RangeSlider
+            title='DNase Z-Score'
+            width={250}
+            defaultStart={DNaseStart}
+            defaultEnd={DNaseEnd}
+            min={-10}
+            max={10}
+            minDistance={1}
+            step={0.1}
+            //How do I correctly type this? Doesn't like Number[]
+            onChange={(value: any) => {
+              setDNaseStart(value[0])
+              setDNaseEnd(value[1])
+            }} />
+          <RangeSlider
+            title='H3K4me3 Z-Score'
+            width={250}
+            defaultStart={H3K4me3Start}
+            defaultEnd={H3K4me3End}
+            min={-10}
+            max={10}
+            minDistance={1}
+            step={0.1}
+            onChange={(value: any) => {
+              setH3K4me3Start(value[0])
+              setH3K4me3End(value[1])
+            }} />
+          <RangeSlider
+            title='H3K27ac Z-Score'
+            width={250}
+            defaultStart={H3K27acStart}
+            defaultEnd={H3K27acEnd}
+            min={-10}
+            max={10}
+            minDistance={1}
+            step={0.1}
+            onChange={(value: any) => {
+              setH3K27acStart(value[0])
+              setH3K27acEnd(value[1])
+            }} />
+          <RangeSlider
+            title='CTCF Z-Score'
+            width={250}
+            defaultStart={CTCFStart}
+            defaultEnd={CTCFEnd}
+            min={-10}
+            max={10}
+            minDistance={1}
+            step={0.1}
+            onChange={(value: any) => {
+              setCTCFStart(value[0])
+              setCTCFEnd(value[1])
+            }} />
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -106,6 +172,14 @@ export default function MainResultsFilters() {
           </Typography>
         </AccordionDetails>
       </Accordion>
+      <Link href={constructURL()}>
+        <Button
+          variant="contained"
+          endIcon={<SendIcon />}
+        >
+          Filter Results
+        </Button>
+      </Link>
     </>
   );
 }
