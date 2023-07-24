f9514d50880cd9b9bbca46cc04e2c6dc6e7785c8
Jordan Velez-Bomba <jordan.velezbomba@gmail.com>
2023-07-24 16:41:13 -0400

    added collapse all, removed outline, and fixed scaling

--- screen2.0/src/app/applets/gene-expression/utils.tsx
+++ screen2.0/src/app/applets/gene-expression/utils.tsx
@@ -1,33 +1,11 @@
-import { Accordion, AccordionDetails, AccordionSummary, Box, ToggleButton, Typography } from "@mui/material"
+import React, { useState } from "react"
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Stack, ToggleButton, Typography } from "@mui/material"
 import { styled } from "@mui/material/styles"
-import { GeneExpression } from "./types"
+import { RIDItem, GeneExpEntry, GeneExpressions } from "./types"
 import { Fragment } from "react"
 import { Range2D, Point2D, linearTransform2D } from "jubilant-carnival"
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
-import { InfoRounded } from "@mui/icons-material"
-
-export type RIDItem = {
-  [id: string]: {
-    ageTitle: string
-    cellType: string
-    logFPKM: number
-    logTPM: number
-    rID: number
-    rawFPKM: number
-    rawTPM: number
-    rep: number
-    tissue: string
-  }[]
-}
-
-export type GeneExpEntry = {
-  value: number
-  cellType: string
-  expID: string
-  rep: number
-  tissue: string
-  color: string
-}
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2"
 
 export const ToggleButtonMean = styled(ToggleButton)(() => ({
   "&.Mui-selected, &.Mui-selected:hover": {
@@ -36,119 +14,137 @@ export const ToggleButtonMean = styled(ToggleButton)(() => ({
   },
 }))
 
-export function PlotGeneExpression(
-  data: GeneExpression,
+export function PlotGeneExpression(props: {
+  data: GeneExpressions,
   range: Range2D,
   dimensions: Range2D,
   RNAtype: string,
   group: string,
   scale: string,
   replicates: string,
-  biosamples_list: string[],
-  cell_components_list: string[]
+}
 ) {
-  console.log(data[RNAtype])
-  let itemsRID: any = data[RNAtype]["itemsByRID"]
+  const [collapse, setCollapse]= useState<{[id: string]: { expand: boolean }}>({})
+
+  let itemsRID: {[id: string]: RIDItem} = props.data[props.RNAtype]["itemsByRID"]
+  let tissues: {[id: string]: { sum: number, values: GeneExpEntry[]}} = {} // dict of ftissues
   let p1: Point2D = { x: 0, y: 0 }
   let max: number = 0
 
-  // let tissues: {[tissue: string]: {
-  //   sum: number,
-  //   values: {
-  //     value: number,
-  //     cellType: string,
-  //     expID: string,
-  //     rep: number,
-  //     tissue: string
-  //   }[]
-  // }} = {}
-  let tissues: any = {} // dict of tissues
-
-  Object.values(data[RNAtype][replicates][group]).map((biosample) => {
+  Object.values(props.data[props.RNAtype][props.replicates][props.group]).map((biosample) => {
     Object.values(biosample["items"]).map((id: string) => {
+      if (!tissues[itemsRID[id]["tissue"]]) tissues[itemsRID[id]["tissue"]] = { sum: 0, values: [] }
       if (tissues[itemsRID[id]["tissue"]]) {
-        tissues[itemsRID[id]["tissue"]].sum += itemsRID[id][scale]
+        tissues[itemsRID[id]["tissue"]].sum += itemsRID[id][props.scale]
         tissues[itemsRID[id]["tissue"]].values.push({
-          value: itemsRID[id][scale],
+          value: itemsRID[id][props.scale],
           cellType: itemsRID[id]["cellType"],
           expID: itemsRID[id]["expID"],
           rep: itemsRID[id]["rep"],
           tissue: itemsRID[id]["tissue"],
           color: biosample["color"],
         })
-      } else {
-        tissues[itemsRID[id]["tissue"]] = {
-          sum: itemsRID[id][scale],
-          values: [
-            {
-              value: itemsRID[id][scale],
-              cellType: itemsRID[id]["cellType"],
-              expID: itemsRID[id]["expID"],
-              rep: itemsRID[id]["rep"],
-              tissue: itemsRID[id]["tissue"],
-              color: biosample["color"],
-            },
-          ],
-        }
-      }
-      if (group === "byTissueMaxFPKM" && tissues[itemsRID[id]["tissue"]].sum > max) max = tissues[itemsRID[id]["tissue"]].sum
-      else if (itemsRID[id][scale] > max) max = itemsRID[id][scale]
+      } 
+
+      if (props.group === "byTissueMaxFPKM" && tissues[itemsRID[id]["tissue"]].sum > max) max = tissues[itemsRID[id]["tissue"]].sum
+      else if (itemsRID[id][props.scale] > max) max = itemsRID[id][props.scale]
     })
   })
 
-  range.x.end = max
-  console.log(range.x.end)
+  props.range.x.end = max
 
   // returns bar plot for a tissue
-  function plotGeneExp(entry: any, index: number, y: number) {
+  const plotGeneExp = (entry: any, index: number, y: number) => {
     let tissue: string = entry[0]
     let info: any = entry[1]
 
     return Object.values(info.values).map((item: GeneExpEntry, i: number) => {
-      p1 = linearTransform2D(range, dimensions)({ x: item.value, y: 0 })
+      p1 = linearTransform2D(props.range, props.dimensions)({ x: item.value, y: 0 })
       return (
         <Fragment key={i}>
-          <rect x={125} width={p1.x + 125} y={y + i * 20} height={18} fill={item["color"]}>
+          <rect x={125} width={p1.x + 125} y={y + (i * 20)} height="18px" fill={item["color"]}>
             <title>{item.value}</title>
           </rect>
           <text x={p1.x + 125 + 150} y={y + i * 20 + 12.5} style={{ fontSize: 12 }}>
             {Number(item.value.toFixed(3)) + " "}
-            {/* {item.value + " "} */}
             <a href={"https://www.encodeproject.org/experiments/" + item.expID}>{item.expID}</a>
             {" " + item.cellType}
           </text>
-          <line x1={125} x2={125} y1={y + i * 20} y2={y + (i * 20 + 18)} stroke="black" />
+          <line x1={125} x2={125} y1={y + (i * 20)} y2={y + (i * 20 + 18)} stroke="black" />
         </Fragment>
       )
     })
   }
 
   let y: number = 0
-  return Object.entries(tissues).map((entry, index: number) => {
-    let info: any = entry[1]
-    y = info.values.length + 20 + 10
-    let view: string = "0 0 1200 " + (info.values.length * 10 + 20)
-    return (
-      // <Box>
-      <Accordion defaultExpanded={true} disableGutters={true} sx={{ padding: 0, outline: "white" }}>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />} sx={{ padding: 0, margin: 0 }}>
-          <Typography variant="h5">{entry[0]}</Typography>
-        </AccordionSummary>
-        <AccordionDetails sx={{ padding: 0 }}>
-          <svg className="graph" aria-labelledby="title desc" role="img" viewBox={view}>
-            <g className="data" data-setname="gene expression plot">
-              <Fragment key={index}>
-                {/* <text x={10} y={y - info.values.length * 20 + 20} style={{ fontSize: 20, fontWeight: "bolder" }}>
-          {entry[0]}
-        </text> */}
-                <line x1={0} x2={900} y1={1} y2={1} stroke="black" />
-                {plotGeneExp(entry, index, y - info.values.length * 20)}
-              </Fragment>
-            </g>
-          </svg>
-        </AccordionDetails>
-      </Accordion>
-      // </Box>
-    )
-  })
+  return (
+    <>
+<Grid2 xs={10} md={10} lg={10} sx={{ alignItems: "right", justifyContent: "right", display: "flex", mr: 4, mt: 2 }}>
+          <Box>
+            <Button onClick={() => {
+              let c: {[id: string]: { expand: boolean }} = {}
+              let uncollapse: boolean = true
+              Object.keys(collapse).map((b: string) => {
+                if (collapse[b].expand) uncollapse = false
+                c[b] = {expand: false}
+              })
+
+              if (uncollapse){
+                Object.keys(collapse).map((b: string) => {
+                  c[b].expand = true
+                })
+              }
+              setCollapse(c)
+            }}>Collapse All</Button>
+          </Box>
+        </Grid2>
+        <Grid2 xs={12} md={12} lg={12} mt={1} ml={2} mr={2}>
+                <Stack>
+      {Object.entries(tissues).map((entry, index: number) => {
+      let info: any = entry[1]
+      y = info.values.length + 20 + 10
+      let view: string = "0 0 1200 " + (info.values.length*20 + 20) 
+      return (
+          <Accordion 
+            key={index} 
+            expanded={collapse[entry[0]] ? collapse[entry[0]].expand : true} 
+            disableGutters={true} 
+            sx={{ padding: 0, ml: "2rem", mr: "2rem" }}
+            
+            >
+              <AccordionSummary 
+                expandIcon={<ExpandMoreIcon />} 
+                sx={{ padding: 0, margin: 0}}
+                onClick={() => {
+                  let tmp: {[id: string]: {expand: boolean}} = {}
+                    Object.entries(tissues).map((x) => {
+                      if (x[0] === entry[0]){
+                        if (collapse[entry[0]] === undefined || collapse[entry[0]].expand) tmp[entry[0]] = {expand: false}
+                        else tmp[entry[0]] = {expand: true}
+                      }
+                      else {
+                        tmp[x[0]] = { expand: collapse[x[0]] !== undefined ? collapse[x[0]].expand : true }
+                      }
+                    })
+                    setCollapse(tmp)
+                }} 
+              >
+                <Typography variant="h5">{entry[0]}</Typography>
+              </AccordionSummary>
+              <AccordionDetails sx={{ padding: 0}}>
+                <svg className="graph" aria-labelledby="title desc" role="img" viewBox={view}>
+                  <g className="data" data-setname="gene expression plot">
+                    <line x1={0} x2={900} y1={1} y2={1} stroke="black" />
+                    {plotGeneExp(entry, index, 5)}
+                </g>
+              </svg>
+            </AccordionDetails>
+          </Accordion>
+      )
+    })
+    }
+                </Stack>
+                </Grid2>
+                </>
+  )
 }
