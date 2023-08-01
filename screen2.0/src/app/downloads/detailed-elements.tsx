243846d88f79bb549edeb3f79405e569835b0281
Jonathan Fisher <jpfisher72@gmail.com>
2023-08-01 01:52:04 -0400

    Cleanup tab 1, UI for tab 2

--- screen2.0/src/app/downloads/detailed-elements.tsx
+++ screen2.0/src/app/downloads/detailed-elements.tsx
@@ -1,26 +1,98 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonProps, IconButton, Tooltip, Typography } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import { Box } from "@mui/system";
+import React from "react";
+import InfoIcon from '@mui/icons-material/Info';
+import Config from "../../config.json"
+import { DownloadButton } from "./quick-start";
+import DownloadIcon from '@mui/icons-material/Download';
+import SearchIcon from '@mui/icons-material/Search';
+import { CA_CTCF, CA_H3K4me3, CA_TF, CA_only, PLS, TF_only, dELS, pELS } from "../../common/lib/colors";
 
 interface TabPanelProps {
   children?: React.ReactNode;
   value: number;
 }
 
+const InlineDownloadButton = (props: ButtonProps & { label: string, borderColor: string , search?: boolean}) => {
+  return (
+    <Box borderTop={`0.25rem solid ${props.borderColor}`}>
+      <Button
+        sx={{ textTransform: "none", maxWidth: "9rem" }}
+        variant="text"
+        color="primary"
+        {...props}
+        endIcon={props.search ? <SearchIcon /> : <DownloadIcon />}
+      >
+        {props.label}
+      </Button>
+    </Box>
+
+  )
+}
+
 export function DetailedElements(props: TabPanelProps) {
   const { children, value, ...other } = props;
 
   return (
-    <div
-      role="tabpanel"
-      id={`simple-tabpanel-${1}`}
-      aria-labelledby={`simple-tab-${1}`}
-      {...other}
-    >
+    <React.Fragment>
       {value === 1 &&
-        <Box sx={{ p: 3 }}>
-          <Typography>Detailed Elements</Typography>
-        </Box>
+        <Grid2 container spacing={5} mt={1}>
+          {/* Titles */}
+          <Grid2 xs={6}>
+            <Typography variant="h5">Human (GRCh38/hg38)</Typography>
+            <Typography variant="subtitle1">2,348,854 cCREs • 1,678 cell types</Typography>
+          </Grid2>
+          <Grid2 xs={6}>
+            <Typography variant="h5">Mouse (GRCm38/mm10)</Typography>
+            <Typography variant="subtitle1">926,843 cCREs • 366 cell types</Typography>
+          </Grid2>
+          <Grid2 xs={6}>
+            <DownloadButton href={Config.Downloads.HumanCCREs} label="Download All Human cCREs" />
+          </Grid2>
+          <Grid2 xs={6}>
+            <DownloadButton href={Config.Downloads.MouseCCREs} label="Download All Mouse cCREs" />
+          </Grid2>
+          <Grid2 display={"flex"} flexWrap={"wrap"} justifyContent={"space-between"} xs={6}>
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label="Promoter-like (PLS) \n(47,532)" borderColor={PLS} />
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label="Proximal enhancer-like (pELS) (249,464)" borderColor={pELS}/>
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label="Distal enhancer-like (dELS) (1,469,205)" borderColor={dELS}/>
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label="Chromatin Accessible with CTCF (126,034)" borderColor={CA_CTCF}/>
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label="Chromatin Accessible with H3K4me3 (79,246)" borderColor={CA_H3K4me3}/>
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label="Chromatin Accessible with TF (26,102)" borderColor={CA_TF}/>
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label="Chromatin Accessible Only (245,985)" borderColor={CA_only}/>
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label="TF Only (105,286)" borderColor={TF_only}/>
+            <Box width={"9rem"}></Box>
+          </Grid2>
+          <Grid2 display={"flex"} flexWrap={"wrap"} justifyContent={"space-between"} xs={6}>
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label={`Promoter-like (PLS) (47,532)`} borderColor={PLS} />
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label="Proximal enhancer-like (pELS) (249,464)" borderColor={pELS}/>
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label="Distal enhancer-like (dELS) (1,469,205)" borderColor={dELS}/>
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label="Chromatin Accessible with CTCF (126,034)" borderColor={CA_CTCF}/>
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label="Chromatin Accessible with H3K4me3 (79,246)" borderColor={CA_H3K4me3}/>
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label="Chromatin Accessible with TF (26,102)" borderColor={CA_TF}/>
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label="Chromatin Accessible Only (245,985)" borderColor={CA_only}/>
+            <InlineDownloadButton href={Config.Downloads.HumanCCREs} label="TF Only (105,286)" borderColor={TF_only}/>
+            <Box width={"9rem"}></Box>
+          </Grid2>
+          <Grid2 xs={6}>
+            <Typography variant="h6">Human cCREs by Cell Type</Typography>
+          </Grid2>
+          <Grid2 xs={6}>
+            <Typography variant="h6">Mouse cCREs by Cell Type</Typography>
+          </Grid2>
+          <Grid2 display={"flex"} flexWrap={"wrap"} justifyContent={"space-between"} xs={6}>
+            <InlineDownloadButton search href={Config.Downloads.HumanCCREs} label="Cell Lines (206 Cell Types)" borderColor={"#333333"} />
+            <InlineDownloadButton search href={Config.Downloads.HumanCCREs} label="Adult Primary Cells and Tissues (1093 cell types)" borderColor={"#333333"}/>
+            <InlineDownloadButton search href={Config.Downloads.HumanCCREs} label="Embryonic Tissues (379 cell types)" borderColor={"#333333"}/>
+          </Grid2>
+          <Grid2 display={"flex"} flexWrap={"wrap"} justifyContent={"space-between"} xs={6}>
+            <InlineDownloadButton search href={Config.Downloads.HumanCCREs} label="Cell Lines (27 Cell Types)" borderColor={"#333333"} />
+            <InlineDownloadButton search href={Config.Downloads.HumanCCREs} label="Adult Primary Cells and Tissues (244 cell types)" borderColor={"#333333"}/>
+            <InlineDownloadButton search href={Config.Downloads.HumanCCREs} label="Embryonic Tissues (96 cell types)" borderColor={"#333333"}/>
+          </Grid2>
+        </Grid2>
       }
-    </div>
+    </React.Fragment>
   );
 }
--- screen2.0/src/app/downloads/quick-start.tsx
+++ screen2.0/src/app/downloads/quick-start.tsx
@@ -9,14 +9,15 @@ import {
   Autocomplete,
   TextField,
   Tooltip,
-  Modal
+  Modal,
+  Box
 } from "@mui/material";
 
 import InfoIcon from '@mui/icons-material/Info';
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2"
 import LoadingButton from '@mui/lab/LoadingButton'
+import DownloadIcon from '@mui/icons-material/Download';
 
-import Downloads from "./page";
 import Config from "../../config.json"
 import { useEffect, useMemo, useState } from "react";
 
@@ -42,7 +43,8 @@ function generateBiosampleURL(selected: Biosample): URL {
   return new URL(`https://downloads.wenglab.org/Registry-V4/${r.join("_")}.bed`)
 }
 
-const DownloadButton = (props: ButtonProps & { label: string }) => {
+// This needs to be moved and imports
+export const DownloadButton = (props: ButtonProps & { label: string }) => {
   return (
     <Button
       sx={{ textTransform: "none" }}
@@ -50,6 +52,7 @@ const DownloadButton = (props: ButtonProps & { label: string }) => {
       variant="contained"
       color="primary"
       {...props}
+      endIcon={<DownloadIcon />}
     >
       {props.label}
     </Button>
@@ -107,8 +110,6 @@ function ComboBox(props: {options: Biosample[], label: string, mode: "H-promoter
               .split("\n")
               .filter((x) => x.includes(stringToMatch))
               .join("\n"),
-            // `${result.name}.${props.title}.bed`
-            //Need to customize this file name
             `${selectedBiosample.name}.${props.mode === "H-promoter" || props.mode === "M-promoter" ? "promoters" : props.mode === "H-enhancer" || props.mode === "M-enhancer" ? "enhancers" : "CTCF-bound cCREs"}.bed`
           )
           setToDownload(null)
@@ -140,6 +141,7 @@ function ComboBox(props: {options: Biosample[], label: string, mode: "H-promoter
           onClick={() => setToDownload(generateBiosampleURL(selectedBiosample))}
           variant="contained"
           color="primary"
+          endIcon={<DownloadIcon />}
         >
           {`Download ${props.mode === "H-promoter" || props.mode === "M-promoter" ? "promoters" : props.mode === "H-enhancer" || props.mode === "M-enhancer" ? "enhancers" : "CTCF-bound cCREs"} active in ${selectedBiosample.name.replace(/_/g, " ")}`}
         </LoadingButton>
@@ -165,106 +167,108 @@ export function QuickStart(props: TabPanelProps) {
       aria-labelledby={`simple-tab-${0}`}
     >
       {props.value === 0 &&
-        <Grid2 container spacing={3}>
+        <Grid2 container spacing={5} mt={1}>
           {/* Titles */}
           <Grid2 xsOffset={2} xs={5}>
-            <Typography variant="h5">Human</Typography>
+            <Typography variant="h5">Human (GRCh38/hg38)</Typography>
+            <Typography variant="subtitle1">2,348,854 cCREs • 1,678 cell types</Typography>
           </Grid2>
           <Grid2 xs={5}>
-            <Typography variant="h5">Mouse</Typography>
+            <Typography variant="h5">Mouse (GRCm38/mm10)</Typography>
+            <Typography variant="subtitle1">926,843 cCREs • 366 cell types</Typography>
           </Grid2>
           {/* All cCREs */}
-          <Grid2 xs={2}>
+          <Grid2 xs={2}  borderLeft={"0.375rem solid #06DA93"}>
             <Typography>All cCREs</Typography>
           </Grid2>
           <Grid2 xs={5}>
-            <DownloadButton href={Config.Downloads.HumanCCREs} label="Download All Human cCREs (hg38)"/>
+            <DownloadButton href={Config.Downloads.HumanCCREs} label="Download All Human cCREs"/>
           </Grid2>
           <Grid2 xs={5}>
-            <DownloadButton href={Config.Downloads.MouseCCREs} label="Download All Mouse cCREs (hg38)"/>
+            <DownloadButton href={Config.Downloads.MouseCCREs} label="Download All Mouse cCREs"/>
           </Grid2>
           {/* Promoters */}
-          <Grid2 xs={2}>
-            <Stack direction="row">
-              <Typography>Candidate Promoters</Typography>
-            <Tooltip title={PROMOTER_MESSAGE}>
-              <IconButton>
-                <InfoIcon />
-              </IconButton>
-            </Tooltip>
-            </Stack>
+          <Grid2 xs={2} borderLeft={"0.375rem solid #FF0000"} >
+            <span>
+              <Typography display={"inline"}>Candidate Promoters</Typography>
+              <Tooltip title={PROMOTER_MESSAGE}>
+                <IconButton>
+                  <InfoIcon />
+                </IconButton>
+              </Tooltip>
+            </span>
           </Grid2>
           <Grid2 xs={5}>
             <Stack spacing={2}>
-              <DownloadButton fullWidth href={Config.Downloads.HumanPromoters} variant="contained" color="primary" label="Download Human Candidate Promoters (hg38)"/>
+              <DownloadButton href={Config.Downloads.HumanPromoters} label="Download Human Candidate Promoters"/>
               <ComboBox options={humanPromoters} label="Search for a Biosample" mode="H-promoter" />
             </Stack>
           </Grid2>
           <Grid2 xs={5}>
             <Stack spacing={2}>
-              <DownloadButton href={Config.Downloads.MousePromoters} label="Download Mouse Candidate Promoters (hg38)"/>
+              <DownloadButton href={Config.Downloads.MousePromoters} label="Download Mouse Candidate Promoters"/>
               <ComboBox options={mousePromoters}  label="Search for a Biosample" mode="M-promoter" />
             </Stack>
           </Grid2>
           {/* Enhancers */}
-          <Grid2 xs={2}>
-            <Stack direction="row">
-              <Typography>Candidate Enhancers</Typography>
+          <Grid2 xs={2} borderLeft={"0.375rem solid #FFCD00"}>
+            <span>
+              <Typography display={"inline"}>Candidate Enhancers</Typography>
             <Tooltip title={ENHANCER_MESSAGE}>
               <IconButton>
                 <InfoIcon />
               </IconButton>
             </Tooltip>
-            </Stack>
+            </span>
           </Grid2>
           <Grid2 xs={5}>
             <Stack spacing={2}>
-              <DownloadButton href={Config.Downloads.HumanEnhancers} label="Download Human Candidate Enhancers (hg38)"/>
+              <DownloadButton href={Config.Downloads.HumanEnhancers} label="Download Human Candidate Enhancers"/>
               <ComboBox options={humanEnhancers} label="Search for a Biosample" mode="H-enhancer" />
             </Stack>
           </Grid2>
           <Grid2 xs={5}>
             <Stack spacing={2}>
-              <DownloadButton href={Config.Downloads.MouseEnhancers} label="Download Mouse Candidate Enhancers (hg38)"/>
+              <DownloadButton href={Config.Downloads.MouseEnhancers} label="Download Mouse Candidate Enhancers"/>
               <ComboBox options={mouseEnhancers} label="Search for a Biosample" mode="M-enhancer" />
             </Stack>
           </Grid2>
           {/* CTCF-Bound */}
-          <Grid2 xs={2}>
-            <Stack direction="row">
-              <Typography>CTCF-Bound</Typography>
+          <Grid2 xs={2} borderLeft={"0.375rem solid #00B0F0"}>
+            <span>
+              <Typography display={"inline"}>CTCF-Bound</Typography>
             <Tooltip title={CTCF_MESSAGE}>
               <IconButton>
                 <InfoIcon />
               </IconButton>
             </Tooltip>
-            </Stack>
+            </span>
           </Grid2>
           <Grid2 xs={5}>
             <Stack spacing={2}>
-              <DownloadButton href="https://downloads.wenglab.org/Registry-V4/GRCh38-CTCF.bed" label="Download Human CTCF-Bound cCREs (hg38)"/>
+              <DownloadButton href="https://downloads.wenglab.org/Registry-V4/GRCh38-CTCF.bed" label="Download Human CTCF-Bound cCREs"/>
               <ComboBox options={humanCTCF} label="Search for a Biosample" mode="H-ctcf" />
             </Stack>
           </Grid2>
           <Grid2 xs={5}>
             <Stack spacing={2}>
-              <DownloadButton href={Config.Downloads.MouseCTCF} label="Download Mouse CTCF-Bound cCREs (hg38)"/>
+              <DownloadButton href={Config.Downloads.MouseCTCF} label="Download Mouse CTCF-Bound cCREs"/>
               <ComboBox options={mouseCTCF} label="Search for a Biosample" mode="M-ctcf" />
             </Stack>
           </Grid2>
           {/* Gene Links */}
-          <Grid2 xs={2}>
-            <Stack direction="row">
-              <Typography>Gene Links</Typography>
+          <Grid2 xs={2} borderLeft={"0.375rem solid #A872E5"}>
+            <span>
+              <Typography display={"inline"}>Gene Links</Typography>
             <Tooltip title={LINK_MESSAGE}>
               <IconButton>
                 <InfoIcon />
               </IconButton>
             </Tooltip>
-            </Stack>
+            </span>
           </Grid2>
           <Grid2 xs={5}>
-            <DownloadButton href={Config.Downloads.HumanGeneLinks} label="Download Human cCRE-Gene Links (hg38)"/>
+            <DownloadButton href={Config.Downloads.HumanGeneLinks} label="Download Human cCRE-Gene Links"/>
           </Grid2>
         </Grid2>
       }
