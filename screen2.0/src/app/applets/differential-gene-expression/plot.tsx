bad5149a191374d8abf81047dad968c9b99f0dd2
Jordan Velez-Bomba <jordan.velezbomba@gmail.com>
2023-08-02 19:53:12 -0400

    added x-axis label / chr location, cleaned up

--- screen2.0/src/app/applets/differential-gene-expression/plot.tsx
+++ screen2.0/src/app/applets/differential-gene-expression/plot.tsx
@@ -6,11 +6,11 @@ import { Gene, cCREZScore } from "./types"
 
 /**
  * Plots difference in z-score and gene expression
- * @param props 
+ * @param props
  * @returns svg of plotted z-score of ccres and log2 fold change
  */
 export function PlotDifferentialExpression(props: {
-  title: {ct1: { name: string, expID: string }, ct2: { name: string, expID: string}}
+  title: { ct1: { name: string; expID: string }; ct2: { name: string; expID: string } }
   chromosome: string
   range: Range2D
   dimensions: Range2D
@@ -22,13 +22,14 @@ export function PlotDifferentialExpression(props: {
   toggleFC: boolean
   toggleccres: boolean
   toggleGenes: boolean
+  togglePCT: { TF: boolean; CA: boolean; "CA-CTCF": boolean; "CA-H3K4me3": boolean; dELS: boolean; pELS: boolean }
   setRange: React.Dispatch<React.SetStateAction<Range2D>>
 }) {
-    let data: Gene[] = [...props.data_genes.gene]
-    data = data.sort((a, b) => (a.coordinates.start < b.coordinates.start ? -1 : 1))
-    let tmp: number = 408 / data.length
+  let data: Gene[] = [...props.data_genes.gene]
+  data = data.sort((a, b) => (a.coordinates.start < b.coordinates.start ? -1 : 1))
+  let tmp: number = 408 / data.length
   return (
-    <svg className="graph" aria-labelledby="title desc" role="img" viewBox="0 0 1000 550">
+    <svg className="graph" aria-labelledby="title desc" role="img" viewBox="0 0 1000 535">
       <g className="x-grid grid" id="xGrid">
         <line x1="100" x2="900" y1="450" y2="450"></line>
       </g>
@@ -38,16 +39,16 @@ export function PlotDifferentialExpression(props: {
       <g className="legend" transform="translate(0,0)">
         <circle cx={735} cy={20} r="8" fill={H3K27ac}></circle>
         <text style={{ fontSize: 11 }} x={750} y={24}>
-          H3k427ac
+          Enhancer-like Signature
         </text>
         <circle cx={535} cy={20} r="8" fill={H3K4me3}></circle>
         <text style={{ fontSize: 11 }} x={550} y={24}>
-          H3K4me3
+          Promoter-like Signature
         </text>
-        <text x="70" y="24" style={{ fontSize: 12, fontStyle: "italic" }}>
-          {props.chromosome}
+        <text x="100" y="24" style={{ fontSize: 10, fontStyle: "italic" }}>
+          {props.chromosome + ":" + props.range.x.start.toLocaleString("en-US") + "-" + props.range.x.end.toLocaleString("en-US")}
         </text>
-        <text x={400} y={525} style={{ fontSize: 15 }}>
+        <text x={400} y={515} style={{ fontSize: 15 }}>
           Coordinates (base pairs)
         </text>
       </g>
@@ -57,8 +58,8 @@ export function PlotDifferentialExpression(props: {
       </g>
       <g className="labels y-labels">
         <SetRange_y
-              title={props.title}
-              range={props.range}
+          title={props.title}
+          range={props.range}
           dimensions={props.dimensions}
           ct1={props.ct1}
           ct2={props.ct2}
@@ -68,12 +69,18 @@ export function PlotDifferentialExpression(props: {
         />
         <line x1="100" y1="50" x2="100" y2="450" stroke="black"></line>
         <line x1="900" y1="50" x2="900" y2="450" stroke="#549623"></line>
-        {!props.toggleFC ? <></> : (
+        {!props.toggleFC ? (
+          <></>
+        ) : (
           <g transform="translate(890,240) rotate(-90)">
-            <text style={{ fontSize: 12, fill: "#549623" }}>log2 gene expression fold change</text>
+            <text style={{ fontSize: 12, fill: "#549623" }}>
+              log2 gene expression fold change
+            </text>
           </g>
         )}
-        {!props.toggleccres ? <></> : (
+        {!props.toggleccres ? (
+          <></>
+        ) : (
           <text x="110" y="50" style={{ fontSize: 12, writingMode: "vertical-lr" }}>
             change in cCRE Z-score
           </text>
@@ -86,7 +93,9 @@ export function PlotDifferentialExpression(props: {
                             <BarPoint point={point} i={i} range={props.range} dimensions={props.dimensions} />
                           )
                         )} */}
-        {!props.toggleccres ? <></> : (
+        {!props.toggleccres ? (
+          <></>
+        ) : (
           props.data_ct1.cCRESCREENSearch.map((point: cCREZScore, i: number) => (
             <Point
               key={i}
@@ -98,12 +107,23 @@ export function PlotDifferentialExpression(props: {
               setRange={props.setRange}
               ct1={props.ct1}
               ct2={props.ct2}
+              togglePCT={props.togglePCT}
             />
           ))
         )}
-        {!props.toggleGenes ? <></> : (
+        {!props.toggleGenes ? (
+          <></>
+        ) : (
           data.map((point: Gene, i: number) => (
-            <GenePoint key={i} point={point} i={i} range={props.range} dimensions={props.dimensions} toggleGenes={props.toggleGenes} size={tmp} />
+            <GenePoint
+              key={i}
+              point={point}
+              i={i}
+              range={props.range}
+              dimensions={props.dimensions}
+              toggleGenes={props.toggleGenes}
+              size={tmp}
+            />
           ))
         )}
       </g>
@@ -113,13 +133,13 @@ export function PlotDifferentialExpression(props: {
 
 /**
  * PLots genes within coordinate range
- * @param {Gene[], Range2D, Range2D} props  
+ * @param {Gene[], Range2D, Range2D} props
  * @returns svg of plotted genes w/ length
  */
-export function PlotGenes(props: { data_genes: { gene: Gene[] }, range: Range2D; dimensions: Range2D }) {
-    let data: Gene[] = [...props.data_genes.gene]
-    data = data.sort((a, b) => (a.coordinates.start < b.coordinates.start ? -1 : 1))
-    let tmp: number = 300 / data.length
+export function PlotGenes(props: { data_genes: { gene: Gene[] }; range: Range2D; dimensions: Range2D }) {
+  let data: Gene[] = [...props.data_genes.gene]
+  data = data.sort((a, b) => (a.coordinates.start < b.coordinates.start ? -1 : 1))
+  let tmp: number = 300 / data.length
   return (
     <svg
       className="graph"
@@ -141,7 +161,7 @@ export function PlotGenes(props: { data_genes: { gene: Gene[] }, range: Range2D;
       </g>
       <g className="data">
         {data.map((point: Gene, i: number) => (
-          <GenePoint key={i} point={point} i={i} range={props.range} dimensions={props.dimensions} toggleGenes={false} size={tmp}/>
+          <GenePoint key={i} point={point} i={i} range={props.range} dimensions={props.dimensions} toggleGenes={false} size={tmp} />
         ))}
       </g>
     </svg>
