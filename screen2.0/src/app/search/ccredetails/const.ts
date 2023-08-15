8f8cce2caad1bedac22167993d00753a55d82249
Jordan Velez-Bomba <jordan.velezbomba@gmail.com>
2023-08-15 07:53:37 -0400

    updated browser

--- screen2.0/src/app/search/ccredetails/const.ts
+++ screen2.0/src/app/search/ccredetails/const.ts
@@ -1,3 +1,5 @@
-export const RampageToolTipInfo = "RAMPAGE (RNA Annotiation and Mapping of Promoters for Analysis of Gene Expression) data allows for the annotation of and expression at transcription start sites with nucleotide precision. Here, we display the sum of signal (from ENCODE Data Coordination Center's RAMPAGE pipeline) in a +-50bp window around GENCODE V19 annotated TSSs for the cRE's nearest protein coding gene. As, this signal is strand specific, we display signal from both the + and - strands for each experiment."
+export const RampageToolTipInfo =
+  "RAMPAGE (RNA Annotiation and Mapping of Promoters for Analysis of Gene Expression) data allows for the annotation of and expression at transcription start sites with nucleotide precision. Here, we display the sum of signal (from ENCODE Data Coordination Center's RAMPAGE pipeline) in a +-50bp window around GENCODE V19 annotated TSSs for the cRE's nearest protein coding gene. As, this signal is strand specific, we display signal from both the + and - strands for each experiment."
 
-export const GenomeBrowserTooltip = "Use the handles at left to drag items and change the order in which they will display in the browser. \nNote: For the best UCSC performance, choose <10 cell types."
+export const GenomeBrowserTooltip =
+  "Use the handles at left to drag items and change the order in which they will display in the browser. \nNote: For the best UCSC performance, choose <10 cell types."
--- screen2.0/src/app/search/ccredetails/genomebrowser.tsx
+++ screen2.0/src/app/search/ccredetails/genomebrowser.tsx
@@ -1,16 +1,18 @@
 import React, { useEffect } from "react"
 import { Button } from "@mui/base"
-import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, Paper } from "@mui/material"
+import { Box, Checkbox, FormControlLabel, FormGroup, Paper, Typography } from "@mui/material"
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2"
 import { DataTable } from "@weng-lab/psychscreen-ui-components"
 import { ErrorMessage, createLink } from "../../../common/lib/utility"
 import { useState } from "react"
 import { cellTypeInfoArr } from "../../applets/differential-gene-expression/types"
+import CheckIcon from '@mui/icons-material/Check'
+import { GenomeBrowserTooltip } from './const';
 
 export const ConfigureGenomeBrowser = (props: {assembly: string}) => {
     const [loading, setLoading] = useState<boolean>(true)
     const [cellTypes, setCellTypes] = useState<cellTypeInfoArr>()
-    const [checked, setChecked] = useState<{[id: string]: boolean}>(null)
+    const [checked, setChecked] = useState<{[id: string]: boolean}>()
 
     // fetch list of cell types
   useEffect(() => {
@@ -51,10 +53,14 @@ export const ConfigureGenomeBrowser = (props: {assembly: string}) => {
                     control={<Checkbox checked={checked ? checked[biosample] : false} 
                     onClick={() => {
                       let x: {[id: string]: boolean} = {}
-                      Object.entries(checked).map((entries) => {
-                        x[entries[0]] = entries[1]
-                      })
-                      x[biosample] = x[biosample] ? false : true
+                      if (checked)
+                        Object.entries(checked).map((entries) => {
+                          if (x[biosample] === entries[1])
+                            x[biosample] = x[biosample] ? false : true
+                          else x[entries[0]] = entries[1]
+                        })
+                      // x[biosample] = x[biosample] ? false : true
+                      setChecked(x)
                     }} 
                   />}
                 ></FormControlLabel>
@@ -66,6 +72,14 @@ export const ConfigureGenomeBrowser = (props: {assembly: string}) => {
       <Box>
         <Paper>
             <Grid2 container spacing={3}>
+              <Grid2 xs={12} m={1}>
+                <Paper>
+                  <Typography>Selected Biosamples</Typography>
+                  <Typography>
+                    {GenomeBrowserTooltip}
+                  </Typography>
+                </Paper>
+              </Grid2>
                 <Grid2 xs={12}>
             {cellTypes && cellTypes["cellTypeInfoArr"] && (
             <DataTable
@@ -73,11 +87,11 @@ export const ConfigureGenomeBrowser = (props: {assembly: string}) => {
                       page={0}
                       rows={cellTypes["cellTypeInfoArr"]}
                       columns={[
-                        { header: "Check", value: (row: any) => row.biosample_summary, render: (row: any) => UCSCCheck(row.biosample_summary)}
+                        { header: "Check", value: (row: any) => row.biosample_summary, render: (row: any) => UCSCCheck(row.biosample_summary)},
                         { header: "Cell Type", value: (row: any) => row.biosample_summary },
                         { header: "Experiment", value: (row: any) => row.expID, render: (row: any) => createLink("https://encodeproject.org/experiments/", row.expID)},
                         { header: "Tissue", value: (row: any) => row.tissue },
-                        { header: "RNA-seq", value: (row: any) => row.rnaseq }
+                        { header: "RNA-seq", value: (row: any) => row.rnaseq, render: (row: any) => <CheckIcon /> }
                       ]}
                       onRowClick={(row: any) => {
                         // if (rowHighlight) setRowHighlight([rowHighlight[0], row])
@@ -91,6 +105,5 @@ export const ConfigureGenomeBrowser = (props: {assembly: string}) => {
             </Grid2>
         </Paper>
       </Box>
-        
     )
 }
