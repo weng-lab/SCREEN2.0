a34778437b433b207d0adc4e3123185cf76f645a
Jordan Velez-Bomba <jordan.velezbomba@gmail.com>
2023-08-18 09:28:32 -0400

    updated table

--- screen2.0/src/app/search/ccredetails/genomebrowser.tsx
+++ screen2.0/src/app/search/ccredetails/genomebrowser.tsx
@@ -7,34 +7,70 @@ import { ErrorMessage, createLink } from "../../../common/lib/utility"
 import { useState } from "react"
 import { cellTypeInfoArr } from "../../applets/differential-gene-expression/types"
 import CheckIcon from '@mui/icons-material/Check'
-import { GenomeBrowserTooltip } from './const';
+import { GenomeBrowserTooltip } from './const'
+import { enhancerYellow } from "../../../common/lib/colors"
 
-export const ConfigureGenomeBrowser = (props: {assembly: string}) => {
+export const ConfigureGenomeBrowser = (props: {assembly: string, chromosome: string}) => {
     const [loading, setLoading] = useState<boolean>(true)
     const [cellTypes, setCellTypes] = useState<cellTypeInfoArr>()
     const [checked, setChecked] = useState<{[id: string]: boolean}>()
+    const [range, setRange] = useState<number[]>([0, 100000])
 
     // fetch list of cell types
-  useEffect(() => {
-    fetch("https://storage.googleapis.com/gcp.wenglab.org/newV4/" + props.assembly + ".json")
-      .then((response) => {
-        if (!response.ok) {
-          // throw new Error(response.statusText)
-          return <ErrorMessage error={new Error(response.statusText)} />
-        }
-        return response.json()
+    useEffect(() => {
+      fetch("https://storage.googleapis.com/gcp.wenglab.org/newV4/" + props.assembly + ".json")
+        .then((response) => {
+          if (!response.ok) {
+            // throw new Error(response.statusText)
+            return <ErrorMessage error={new Error(response.statusText)} />
+          }
+          return response.json()
+        })
+        .then((data) => {
+          setCellTypes(data)
+          setLoading(false)
+        })
+        .catch((error: Error) => {
+          // logging
+          // throw error
+          return <ErrorMessage error={error} />
+        })
+      setLoading(true)
+    }, [])
+    
+    // fetch list of cell types
+    useEffect(() => {
+      fetch("https://storage.googleapis.com/gcp.wenglab.org/newV4/" + props.assembly + ".json", {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          assembly: props.assembly,
+          chromosome: props.chromosome,
+          end: range[1],
+          start: range[0],
+        }),
       })
-      .then((data) => {
-        setCellTypes(data)
-        setLoading(false)
-      })
-      .catch((error: Error) => {
-        // logging
-        // throw error
-        return <ErrorMessage error={error} />
-      })
-    setLoading(true)
-  }, [])
+        .then((response) => {
+          if (!response.ok) {
+            // throw new Error(response.statusText)
+            return <ErrorMessage error={new Error(response.statusText)} />
+          }
+          return response.json()
+        })
+        .then((data) => {
+          setCellTypes(data)
+          setLoading(false)
+        })
+        .catch((error: Error) => {
+          // logging
+          // throw error
+          return <ErrorMessage error={error} />
+        })
+      setLoading(true)
+    }, [props.assembly, props.chromosome, range])
 
     const UCSCButton = () => {
         return (
@@ -68,21 +104,9 @@ export const ConfigureGenomeBrowser = (props: {assembly: string}) => {
         )
     }
 
-    return (
-      <Box>
-        <Paper>
-            <Grid2 container spacing={3}>
-              <Grid2 xs={12} m={1}>
-                <Paper>
-                  <Typography>Selected Biosamples</Typography>
-                  <Typography>
-                    {GenomeBrowserTooltip}
-                  </Typography>
-                </Paper>
-              </Grid2>
-                <Grid2 xs={12}>
-            {cellTypes && cellTypes["cellTypeInfoArr"] && (
-            <DataTable
+    const TableView = () => {
+      return (
+        <DataTable
                     //   highlighted={rowHighlight ? rowHighlight[1] : false}
                       page={0}
                       rows={cellTypes["cellTypeInfoArr"]}
@@ -100,7 +124,69 @@ export const ConfigureGenomeBrowser = (props: {assembly: string}) => {
                       sortDescending={true} 
                       searchable={true}
                     />
-                )}
+      )
+    }
+
+    const BrowserView = () => {
+      return (
+        <Box>
+          <svg>
+            <text x={110} y={10}>Enh</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>EnhG</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>EnhLo</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>EnhPois</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>EnhPr</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>Het</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>Quies</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>Quies3</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>Quies4</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>QuiesG</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>ReprPC</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>ReprPCW</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>Tss</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>TssBiv</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>TssFink</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>Tx</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+            <text x={110} y={10}>TxWk</text>
+            <rect height={1} width={1} x={100} fill={enhancerYellow} />
+          </svg>
+        </Box>
+      )
+    }
+
+    return (
+      <Box>
+        <Paper>
+            <Grid2 container spacing={3}>
+              <Grid2 xs={12} m={1}>
+                <Paper>
+                  <Typography>Selected Biosamples</Typography>
+                  <Typography>
+                    {GenomeBrowserTooltip}
+                  </Typography>
+                </Paper>
+              </Grid2>
+                <Grid2 xs={12}>
+                  {cellTypes && cellTypes["cellTypeInfoArr"] && <TableView />}
+                </Grid2>
+                <Grid2 xs={12}>
+                  <BrowserView />
                 </Grid2>
             </Grid2>
         </Paper>
