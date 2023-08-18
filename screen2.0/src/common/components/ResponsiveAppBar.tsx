fae9a8e109eaaed47787637cf87c7cad42e2e635
Jonathan Fisher <49597360+jpfisher72@users.noreply.github.com>
2023-08-18 16:19:10 -0400

    Downloads (#100)
    * Quick Start Downloads
    
    * Fetch Data, Icon
    
    * Biosample autocompletes filled
    
    * Biosample downloads, maybe bugged
    
    * Biosample downloads working
    
    * Tooltips and named download
    
    * Button w/ loading
    
    * Cleanup tab 1, UI for tab 2
    
    * Correct Links, styling
    
    * Lots of small things, accessing data in new tab
    
    * File downloads in modals, all 6 up
    
    * Theme, Icons, Styling
    
    * Fixing Errors
    
    * Data Matrices Initial UI
    
    * Fetching data, conserving matrices tab state
    
    * Data updating, empty plot
    
    * Fix Merge from Main
    
    * Radio Buttons Working
    
    * Biosample view, downloads, default dnase
    
    * Legend, selected biosample tables
    
    * Lots of small changes
    
    * Fix zoom bug, matrices file cleanup
    
    * Cleanup, responsiveness
    
    * Reset theme changes
    
    * More Cleanup
    
    * Attempt to sync package with main
    
    * Update package.json
    
    * Small update
    
    * Sync lockfile

--- screen2.0/src/common/components/ResponsiveAppBar.tsx
+++ screen2.0/src/common/components/ResponsiveAppBar.tsx
@@ -42,11 +42,9 @@ import nextConfig from "../../../next.config"
 import screenIcon from "../../../public/screenIcon.png"
 import { defaultTheme } from "../lib/themes"
 
-// CLICKING ON LINKS ONCE THE POPUP IS OPEN IS BROKEN!!!
-
 /*  
   Links for the AppBar. If adding another page with subpages, you need to add another 
-  useState() hook for positionong, and add extra if case in open/close handlers 
+  useState() hook for positioning, and add extra if case in open/close handlers 
 */
 const pageLinks = [
   {
@@ -85,10 +83,10 @@ const pageLinks = [
 
 function ResponsiveAppBar() {
   const [open, setState] = React.useState<boolean>(false)
-  // Hamburger Menu, deals with setting it's position
+  // Hamburger Menu, deals with setting its position
   const [anchorElNav_Hamburger, setAnchorElNav_Hamburger] = React.useState<null | HTMLElement>(null)
 
-  // Hover dropdowns, deals with setting it's position
+  // Hover dropdowns, deals with setting its position
   const [anchorElNav_Dropdown0, setAnchorElNav_Dropdown0] = React.useState<null | HTMLElement>(null)
   const [anchorElNav_Dropdown1, setAnchorElNav_Dropdown1] = React.useState<null | HTMLElement>(null)
 
@@ -120,12 +118,12 @@ function ResponsiveAppBar() {
     }
   }
 
-  const toggleDrawer = (open) => (event) => {
-    if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
-      return
-    }
-    setState(open)
-  }
+  // const toggleDrawer = (open) => (event) => {
+  //   if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
+  //     return
+  //   }
+  //   setState(open)
+  // }
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -279,7 +277,7 @@ function ResponsiveAppBar() {
               <HeaderSearch />
             </Box>
             {/* Settings */}
-            <Box sx={{ ml: 3, flexGrow: 0 }}>
+            {/* <Box sx={{ ml: 3, flexGrow: 0 }}>
               <IconButton
                 edge="start"
                 color="inherit"
@@ -305,7 +303,7 @@ function ResponsiveAppBar() {
                   <Typography variant="h6">Settings</Typography>
                 </Box>
               </Box>
-            </Drawer>
+            </Drawer> */}
           </Toolbar>
         </Container>
       </AppBar>
