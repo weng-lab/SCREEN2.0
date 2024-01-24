b737d5503dc8dab8b12f6c8963b0e8de7dc092b0
Jonathan Fisher <jpfisher72@gmail.com>
2024-01-24 15:35:10 -0500

    Description and success text

--- screen2.0/src/app/about/page.tsx
+++ screen2.0/src/app/about/page.tsx
@@ -68,81 +68,83 @@ export default function About() {
           <Typography variant="h3">About SCREEN</Typography>
           <Typography variant="h5">Search Candidate cis-Regulatory Elements by ENCODE</Typography>
         </Grid2>
-        <Grid2 xs={12} id="contact-us">
+        <Grid2 xs={12} lg={8} id="contact-us">
           <Typography variant="h3">Contact Us</Typography>
           <Typography mb={1} variant="body1">Send us a message and we&apos;ll be in touch</Typography>
+          <Typography mb={1} variant="body1">As this is a beta site, we would greatly appreciate any feedback you may have. Knowing how our users are using the site and documenting issues they may have are important to make this resource better and easier to use.</Typography>
           <Stack direction="row">
             <Typography mb={1} variant="body1">If you&apos;re experiencing an error/bug, feel free to&nbsp;</Typography>
             <a href="https://github.com/weng-lab/SCREEN2.0/issues"><u><Typography mb={1} color={"primary"} variant="body1">submit an issue on Github</Typography></u></a>
           </Stack>
-          {success ?
-            <Typography>Submitted successfully, thank you!</Typography>
-            :
-            <Box
-              component="form"
-              ref={form}
-              // onSubmit={(event) => handleSubmit(event)}
-              id="contact-us"
-              sx={{
-                '& > :not(style)': { width: '50ch' },
+          <Stack direction="row">
+          <Typography mb={1} variant="body1">If you would like to send an attachment, feel free to email us directly at&nbsp;</Typography>
+            <a href="mailto:encode-screen@googlegroups.com"><u><Typography mb={1} color={"primary"} variant="body1">encode-screen@googlegroups.com</Typography></u></a>
+          </Stack>
+          <Box
+            component="form"
+            ref={form}
+            // onSubmit={(event) => handleSubmit(event)}
+            id="contact-us"
+            sx={{
+              '& > :not(style)': { width: '50ch' },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              required
+              value={contactName}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setContactName(event.target.value);
               }}
-              noValidate
-              autoComplete="off"
+              error={error.name}
+              name="user_name"
+              type="text"
+              sx={{ display: 'block', mb: 1 }}
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+            />
+            <TextField
+              required
+              value={contactEmail}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setContactEmail(event.target.value);
+              }}
+              error={error.email || contactEmail !== '' && !isValidEmail(contactEmail)}
+              helperText={error.email && "Please enter a valid email"}
+              name="user_email"
+              type="email"
+              sx={{ display: 'block', mb: 1 }}
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+            />
+            <TextField
+              required
+              value={contactMessage}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setContactMessage(event.target.value);
+              }}
+              error={error.message}
+              name="message"
+              type="text"
+              fullWidth
+              rows={4}
+              sx={{ display: 'block' }}
+              multiline id="outlined-basic"
+              label="Message"
+              variant="outlined"
+            />
+            <Button
+              sx={{ mt: 1 }}
+              variant="contained"
+              onClick={handleSubmit}
             >
-              <TextField
-                required
-                value={contactName}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  setContactName(event.target.value);
-                }}
-                error={error.name}
-                name="user_name"
-                type="text"
-                sx={{ display: 'block', mb: 1 }}
-                id="outlined-basic"
-                label="Name"
-                variant="outlined"
-              />
-              <TextField
-                required
-                value={contactEmail}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  setContactEmail(event.target.value);
-                }}
-                error={error.email || contactEmail !== '' && !isValidEmail(contactEmail)}
-                helperText={error.email && "Please enter a valid email"}
-                name="user_email"
-                type="email"
-                sx={{ display: 'block', mb: 1 }}
-                id="outlined-basic"
-                label="Email"
-                variant="outlined"
-              />
-              <TextField
-                required
-                value={contactMessage}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  setContactMessage(event.target.value);
-                }}
-                error={error.message}
-                name="message"
-                type="text"
-                fullWidth
-                rows={4}
-                sx={{ display: 'block' }}
-                multiline id="outlined-basic"
-                label="Message"
-                variant="outlined"
-              />
-              <Button
-                sx={{ mt: 1 }}
-                variant="contained"
-                onClick={handleSubmit}
-              >
-                Submit
-              </Button>
-            </Box>
-          }
+              Submit
+            </Button>
+          </Box>
+          {success && <Typography>Submitted successfully, thank you!</Typography>}
         </Grid2>
       </Grid2>
     </main>
