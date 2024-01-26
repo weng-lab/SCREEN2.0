fa2a7106589600770387c24f80475674879ffb00
Jonathan Fisher <jpfisher72@gmail.com>
2024-01-26 10:40:44 -0500

    Better formatting

--- screen2.0/src/app/about/page.tsx
+++ screen2.0/src/app/about/page.tsx
@@ -72,12 +72,14 @@ export default function About() {
           <Typography variant="h3">Contact Us</Typography>
           <Typography mb={1} variant="body1">Send us a message and we&apos;ll be in touch</Typography>
           <Typography mb={1} variant="body1">As this is a beta site, we would greatly appreciate any feedback you may have. Knowing how our users are using the site and documenting issues they may have are important to make this resource better and easier to use.</Typography>
-          <Typography display={"inline"} variant="body1">If you&apos;re experiencing an error/bug, feel free to&nbsp;</Typography>
-          <Link display={"inline"} href="https://github.com/weng-lab/SCREEN2.0/issues">submit an issue on Github.</Link>
-          <br/>
-          <Typography display={"inline"} variant="body1">If you would like to send an attachment, feel free to email us directly at&nbsp;</Typography>
-          <Link display={"inline"} href="mailto:encode-screen@googlegroups.com">encode-screen@googlegroups.com</Link>
-          <br/><br/>
+          <Box mb={1}>
+            <Typography display={"inline"} variant="body1">If you&apos;re experiencing an error/bug, feel free to&nbsp;</Typography>
+            <Link display={"inline"} href="https://github.com/weng-lab/SCREEN2.0/issues">submit an issue on Github.</Link>
+          </Box>
+          <Box mb={2}>
+            <Typography display={"inline"} variant="body1">If you would like to send an attachment, feel free to email us directly at&nbsp;</Typography>
+            <Link display={"inline"} href="mailto:encode-screen@googlegroups.com">encode&#8209;screen@googlegroups.com</Link>
+          </Box>
           <Box
             component="form"
             ref={form}
