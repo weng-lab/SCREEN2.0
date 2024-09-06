0df42ec6336fce8ed8193cae040170429177f401
NishiPhalke <nishaphalke@gmail.com>
2024-09-06 12:36:37 -0400

    Deployed 48b1993 with MkDocs version: 1.6.1

--- screen2.0/src/app/downloads/types.ts
+++
@@ -1,9 +0,0 @@
-export type BiosampleUMAP = {
-  name: string
-  displayname: string
-  ontology: string
-  sampleType: string
-  lifeStage: string
-  umap_coordinates: number[]
-  experimentAccession: string
-}
