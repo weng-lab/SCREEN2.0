724d0e9833807c507a3263244ab55d9945150b2c
mlacadie <lacadiem14@gmail.com>
2024-09-27 14:06:28 -0400

    import useMemo

--- screen2.0/src/app/search/_ccredetails/rampage.tsx
+++ screen2.0/src/app/search/_ccredetails/rampage.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useMemo } from "react"
 import Grid from "@mui/material/Grid2"
 import { LoadingMessage } from "../../../common/lib/utility"
 import {  
