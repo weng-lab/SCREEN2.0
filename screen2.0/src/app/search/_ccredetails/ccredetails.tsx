3e79729f138469a3e16abb74d766af5aa28c6f11
NishiPhalke <30417924+NishiPhalke@users.noreply.github.com>
2024-10-07 10:56:06 -0400

    fix logic for class assignment based on zscores in biosample activity… (#459)
    * fix logic for class assignment based on zscores in biosample activity tab
    
    * remove console log

--- screen2.0/src/app/search/_ccredetails/ccredetails.tsx
+++ screen2.0/src/app/search/_ccredetails/ccredetails.tsx
@@ -154,8 +154,8 @@ export const CcreDetails: React.FC<CcreDetailsProps> = ({ accession, region, ass
         <Typography variant="h6">{`${region.chrom}:${region.start.toLocaleString("en-US")}-${region.end.toLocaleString("en-US")}`}</Typography>
       </Stack>
       <Divider sx={{ mb: 2 }} />
-      {page === 0 &&
-        <InSpecificBiosamples accession={accession} assembly={assembly} />
+      {page === 0 && nearest3AndLinkedGenes &&
+        <InSpecificBiosamples accession={accession} assembly={assembly} distanceToTSS={nearest3AndLinkedGenes.nearbyGenes.sort((a,b)=>a.distanceToTSS-b.distanceToTSS)[0].distanceToTSS}/>
       }
       {(page === 1 && assembly !== "mm10") &&
         (loadingLinkedGenes ?
