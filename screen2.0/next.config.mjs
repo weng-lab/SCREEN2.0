14a260053ec39d8abab1c6ab22604b517fe569f0
jpfisher72 <jpfisher72@gmail.com>
2024-12-09 10:39:46 -0500

    Whoops deleted wrong config file

---
+++ screen2.0/next.config.mjs
@@ -0,0 +1,36 @@
+// @ts-check
+ 
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  // output: "export",
+  //Image optimization incompatible with static exports
+  images: { unoptimized: true },
+  devIndicators: {
+    buildActivityPosition: "bottom-right",
+  },
+  generateEtags: false,
+  trailingSlash: false,
+  assetPrefix: '',
+  basePath: '',
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+        // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+        config.resolve.fallback = {
+            fs: false
+        }
+    }
+
+    return config;
+  }
+}
+
+export default nextConfig
