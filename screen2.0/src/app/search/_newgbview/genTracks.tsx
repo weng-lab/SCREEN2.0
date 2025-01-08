53856f6b0c1b44615f9f2026ba9650a67c6c837a
Jair Meza <jair3202@gmail.com>
2025-01-08 17:08:36 -0500

    Genome Browser related bugs (#541)
    * Fixed crash on tab switch
    
    Also gave generated tracks unique but non-random IDs to avoid duplication checks
    
    * Removed dupe track check
    
    Removed duplicate track checks as they are now handled by the package itself. Using unique IDs that are not randomized to keep track of the current tracks.
    
    * Removed comment
    
    Removed a large section of old commented out code.
    
    * Added color to open cCRE tab
    
    Added the correct highlight color when opening a cCRE from the table. The color is based on the class.,
    
    * Styling fixes
    
    Improved styling of the Genome Browser view by reducing white space and reducing font sizes.
    
    * Removed unnecessary client provider
    
    Removed an extra Apollo Client provider
    
    * Added back dupe check

--- screen2.0/src/app/search/_newgbview/genTracks.tsx
+++ screen2.0/src/app/search/_newgbview/genTracks.tsx
@@ -29,21 +29,21 @@ export function getDefaultTracks(coordinates: { assembly: "GRCh38" | "mm10", chr
     } as TranscriptTrackProps
 
     const defaultMouseTracks = [
-        { ...DefaultBigBed, titleSize, id: randomID(), title: "All cCREs colored by group", displayMode: DisplayMode.DENSE, color: colors.ccre, rowHeight: 12, height: 50, url: "https://downloads.wenglab.org/mm10-cCREs.DCC.bigBed" } as BigBedTrackProps,
-        { ...DefaultBigWig, titleSize, id: randomID(), title: "Aggregated DNase-seq signal, all Registry biosamples", color: colors.dnase, height: 100, url: "https://downloads.wenglab.org/DNase_MM10_ENCODE_DEC2024_merged_nanrm.bigWig" } as BigWigTrackProps,
-        { ...DefaultBigWig, titleSize, id: randomID(), title: "Aggregated H3K4me3 ChIP-seq signal, all Registry biosamples", color: colors.h3k4me3, height: 100, url: "https://downloads.wenglab.org/H3K4me3_MM10_ENCODE_DEC2024_merged_nanrm.bigWig" } as BigWigTrackProps,
-        { ...DefaultBigWig, titleSize, id: randomID(), title: "Aggregated H3K27ac ChIP-seq signal, all Registry biosamples", color: colors.h3k27ac, height: 100, url: "https://downloads.wenglab.org/H3K27ac_MM10_ENCODE_DEC2024_merged_nanrm.bigWig" } as BigWigTrackProps,
-        { ...DefaultBigWig, titleSize, id: randomID(), title: "Aggregated CTCF ChIP-seq signal, all Registry biosamples", color: colors.ctcf, height: 100, url: "https://downloads.wenglab.org/CTCF_MM10_ENCODE_DEC2024_merged_nanrm.bigWig" } as BigWigTrackProps,
-        { ...DefaultBigWig, titleSize, id: randomID(), title: "Aggregated ATAC ChIP-seq signal, all Registry biosamples", color: colors.atac, height: 100, url: "https://downloads.wenglab.org/ATAC_MM10_ENCODE_DEC2024_merged_nanrm.bigWig" } as BigWigTrackProps,
+        { ...DefaultBigBed, titleSize, id: "default-ccre", title: "All cCREs colored by group", displayMode: DisplayMode.DENSE, color: colors.ccre, rowHeight: 12, height: 50, url: "https://downloads.wenglab.org/mm10-cCREs.DCC.bigBed" } as BigBedTrackProps,
+        { ...DefaultBigWig, titleSize, id: "default-dnase", title: "Aggregated DNase-seq signal, all Registry biosamples", color: colors.dnase, height: 100, url: "https://downloads.wenglab.org/DNase_MM10_ENCODE_DEC2024_merged_nanrm.bigWig" } as BigWigTrackProps,
+        { ...DefaultBigWig, titleSize, id: "default-h3k4me3", title: "Aggregated H3K4me3 ChIP-seq signal, all Registry biosamples", color: colors.h3k4me3, height: 100, url: "https://downloads.wenglab.org/H3K4me3_MM10_ENCODE_DEC2024_merged_nanrm.bigWig" } as BigWigTrackProps,
+        { ...DefaultBigWig, titleSize, id: "default-h3k27ac", title: "Aggregated H3K27ac ChIP-seq signal, all Registry biosamples", color: colors.h3k27ac, height: 100, url: "https://downloads.wenglab.org/H3K27ac_MM10_ENCODE_DEC2024_merged_nanrm.bigWig" } as BigWigTrackProps,
+        { ...DefaultBigWig, titleSize, id: "default-ctcf", title: "Aggregated CTCF ChIP-seq signal, all Registry biosamples", color: colors.ctcf, height: 100, url: "https://downloads.wenglab.org/CTCF_MM10_ENCODE_DEC2024_merged_nanrm.bigWig" } as BigWigTrackProps,
+        { ...DefaultBigWig, titleSize, id: "default-atac", title: "Aggregated ATAC ChIP-seq signal, all Registry biosamples", color: colors.atac, height: 100, url: "https://downloads.wenglab.org/ATAC_MM10_ENCODE_DEC2024_merged_nanrm.bigWig" } as BigWigTrackProps,
     ]
 
     const defaultHumanTracks = [
-        { ...DefaultBigBed, titleSize, id: randomID(), title: "All cCREs colored by group", displayMode: DisplayMode.DENSE, color: colors.ccre, rowHeight: 12, height: 50, url: "https://downloads.wenglab.org/GRCh38-cCREs.DCC.bigBed" } as BigBedTrackProps,
-        { ...DefaultBigWig, titleSize, id: randomID(), title: "Aggregated DNase-seq signal, all Registry biosamples", color: colors.dnase, height: 100, url: "https://downloads.wenglab.org/DNAse_All_ENCODE_MAR20_2024_merged.bw" } as BigWigTrackProps,
-        { ...DefaultBigWig, titleSize, id: randomID(), title: "Aggregated H3K4me3 ChIP-seq signal, all Registry biosamples", color: colors.h3k4me3, height: 100, url: "https://downloads.wenglab.org/H3K4me3_All_ENCODE_MAR20_2024_merged.bw" } as BigWigTrackProps,
-        { ...DefaultBigWig, titleSize, id: randomID(), title: "Aggregated H3K27ac ChIP-seq signal, all Registry biosamples", color: colors.h3k27ac, height: 100, url: "https://downloads.wenglab.org/H3K27ac_All_ENCODE_MAR20_2024_merged.bw" } as BigWigTrackProps,
-        { ...DefaultBigWig, titleSize, id: randomID(), title: "Aggregated CTCF ChIP-seq signal, all Registry biosamples", color: colors.ctcf, height: 100, url: "https://downloads.wenglab.org/CTCF_All_ENCODE_MAR20_2024_merged.bw" } as BigWigTrackProps,
-        { ...DefaultBigWig, titleSize, id: randomID(), title: "Aggregated ATAC ChIP-seq signal, all Registry biosamples", color: colors.atac, height: 100, url: "https://downloads.wenglab.org/ATAC_All_ENCODE_MAR20_2024_merged.bw" } as BigWigTrackProps,
+        { ...DefaultBigBed, titleSize, id: "default-ccre", title: "All cCREs colored by group", displayMode: DisplayMode.DENSE, color: colors.ccre, rowHeight: 12, height: 50, url: "https://downloads.wenglab.org/GRCh38-cCREs.DCC.bigBed" } as BigBedTrackProps,
+        { ...DefaultBigWig, titleSize, id: "default-dnase", title: "Aggregated DNase-seq signal, all Registry biosamples", color: colors.dnase, height: 100, url: "https://downloads.wenglab.org/DNAse_All_ENCODE_MAR20_2024_merged.bw" } as BigWigTrackProps,
+        { ...DefaultBigWig, titleSize, id: "default-h3k4me3", title: "Aggregated H3K4me3 ChIP-seq signal, all Registry biosamples", color: colors.h3k4me3, height: 100, url: "https://downloads.wenglab.org/H3K4me3_All_ENCODE_MAR20_2024_merged.bw" } as BigWigTrackProps,
+        { ...DefaultBigWig, titleSize, id: "default-h3k27ac", title: "Aggregated H3K27ac ChIP-seq signal, all Registry biosamples", color: colors.h3k27ac, height: 100, url: "https://downloads.wenglab.org/H3K27ac_All_ENCODE_MAR20_2024_merged.bw" } as BigWigTrackProps,
+        { ...DefaultBigWig, titleSize, id: "default-ctcf", title: "Aggregated CTCF ChIP-seq signal, all Registry biosamples", color: colors.ctcf, height: 100, url: "https://downloads.wenglab.org/CTCF_All_ENCODE_MAR20_2024_merged.bw" } as BigWigTrackProps,
+        { ...DefaultBigWig, titleSize, id: "default-atac", title: "Aggregated ATAC ChIP-seq signal, all Registry biosamples", color: colors.atac, height: 100, url: "https://downloads.wenglab.org/ATAC_All_ENCODE_MAR20_2024_merged.bw" } as BigWigTrackProps,
     ]
 
     const tracks = [geneTrack, ...(coordinates.assembly.toLowerCase() === "mm10" ? defaultMouseTracks : defaultHumanTracks)]
@@ -52,7 +52,6 @@ export function getDefaultTracks(coordinates: { assembly: "GRCh38" | "mm10", chr
 
 export function genBiosampleTracks(biosample: RegistryBiosample, coordinates: { assembly: "GRCh38" | "mm10", chromosome: string, start: number, end: number }, data: any) {
     const tracks = []
-    console.log("data", data, "\nbiosample", biosample)
     const humanBiosamples = data && data.human && data.human.biosamples
     const mouseBiosamples = data && data.mouse && data.mouse.biosamples
 
@@ -61,18 +60,18 @@ export function genBiosampleTracks(biosample: RegistryBiosample, coordinates: { 
             ? mouseBiosamples.find((m) => m.name === biosample.name)
             : humanBiosamples.find((m) => m.name === biosample.name)
     const r = [result.dnase_signal, result.h3k4me3_signal, result.h3k27ac_signal, result.ctcf_signal].filter((x) => !!x)
-    console.log("result", result, "\nr", r)
+
     const bigBedUrl = `https://downloads.wenglab.org/Registry-V4/${r.join("_")}.bigBed`
-    tracks.push({ ...DefaultBigBed, titleSize, id: randomID(), title: "All cCREs colored by group", displayMode: DisplayMode.DENSE, color: colors.ccre, rowHeight: 12, height: 50, url: bigBedUrl } as BigBedTrackProps)
+    tracks.push({ ...DefaultBigBed, titleSize, id: "sample-ccre-" + biosample.name, title: "All cCREs colored by group", displayMode: DisplayMode.DENSE, color: colors.ccre, rowHeight: 12, height: 50, url: bigBedUrl } as BigBedTrackProps)
     if (result.dnase_signal)
-        tracks.push({ ...DefaultBigWig, titleSize, id: randomID(), title: `DNase-seq signal in ${biosample.displayname}`, color: colors.dnase, height: 100, url: `https://www.encodeproject.org/files/${result.dnase_signal}/@@download/${result.dnase_signal}.bigWig` } as BigWigTrackProps)
+        tracks.push({ ...DefaultBigWig, titleSize, id: "sample-dnase-" + biosample.name, title: `DNase-seq signal in ${biosample.displayname}`, color: colors.dnase, height: 100, url: `https://www.encodeproject.org/files/${result.dnase_signal}/@@download/${result.dnase_signal}.bigWig` } as BigWigTrackProps)
     if (result.h3k4me3_signal)
-        tracks.push({ ...DefaultBigWig, titleSize, id: randomID(), title: `H3K4me3 ChIP-seq signal in ${biosample.displayname}`, color: colors.h3k4me3, height: 100, url: `https://www.encodeproject.org/files/${result.h3k4me3_signal}/@@download/${result.h3k4me3_signal}.bigWig` } as BigWigTrackProps)
+        tracks.push({ ...DefaultBigWig, titleSize, id: "sample-h3k4me3-" + biosample.name, title: `H3K4me3 ChIP-seq signal in ${biosample.displayname}`, color: colors.h3k4me3, height: 100, url: `https://www.encodeproject.org/files/${result.h3k4me3_signal}/@@download/${result.h3k4me3_signal}.bigWig` } as BigWigTrackProps)
     if (result.h3k27ac_signal)
-        tracks.push({ ...DefaultBigWig, titleSize, id: randomID(), title: `H3K27ac ChIP-seq signal in ${biosample.displayname}`, color: colors.h3k27ac, height: 100, url: `https://www.encodeproject.org/files/${result.h3k27ac_signal}/@@download/${result.h3k27ac_signal}.bigWig` } as BigWigTrackProps)
+        tracks.push({ ...DefaultBigWig, titleSize, id: "sample-h3k27ac-" + biosample.name, title: `H3K27ac ChIP-seq signal in ${biosample.displayname}`, color: colors.h3k27ac, height: 100, url: `https://www.encodeproject.org/files/${result.h3k27ac_signal}/@@download/${result.h3k27ac_signal}.bigWig` } as BigWigTrackProps)
     if (result.ctcf_signal)
-        tracks.push({ ...DefaultBigWig, titleSize, id: randomID(), title: `CTCF ChIP-seq signal in ${biosample.displayname}`, color: colors.ctcf, height: 100, url: `https://www.encodeproject.org/files/${result.ctcf_signal}/@@download/${result.ctcf_signal}.bigWig` } as BigWigTrackProps)
+        tracks.push({ ...DefaultBigWig, titleSize, id: "sample-ctcf-" + biosample.name, title: `CTCF ChIP-seq signal in ${biosample.displayname}`, color: colors.ctcf, height: 100, url: `https://www.encodeproject.org/files/${result.ctcf_signal}/@@download/${result.ctcf_signal}.bigWig` } as BigWigTrackProps)
     if (result.atac_signal)
-        tracks.push({ ...DefaultBigWig, titleSize, id: randomID(), title: `ATAC ChIP-seq signal in ${biosample.displayname}`, color: colors.atac, height: 100, url: `https://www.encodeproject.org/files/${result.atac_signal}/@@download/${result.atac_signal}.bigWig` } as BigWigTrackProps)
+        tracks.push({ ...DefaultBigWig, titleSize, id: "sample-atac-" + biosample.name, title: `ATAC ChIP-seq signal in ${biosample.displayname}`, color: colors.atac, height: 100, url: `https://www.encodeproject.org/files/${result.atac_signal}/@@download/${result.atac_signal}.bigWig` } as BigWigTrackProps)
     return tracks
 }
