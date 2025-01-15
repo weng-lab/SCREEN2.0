ef7ffde6181d8b59e44a5ff5a8d6b486816e16f0
evac-tcxr <echoudhury@umass.edu>
2025-01-15 15:11:48 -0500

    Updating Transcription at cCREs page with mock data for Pro Cap Peaks table
    Mock data table with correct columns in Transcription at cCREs page for Pro Cap Peaks data table

--- screen2.0/src/app/search/_ccredetails/transcriptionatccres.tsx
+++ screen2.0/src/app/search/_ccredetails/transcriptionatccres.tsx
@@ -1,3 +1,57 @@
+import { DataTable, DataTableColumn } from "@weng-lab/psychscreen-ui-components"
+
+type Row = {
+    id: number;
+    chromosome: string;
+    start: number;
+    stop: number;
+    cellType: string;
+    stringency: string;
+    experimentAccession: string;
+  };
+  
+  const ProCapPeaksColumns: DataTableColumn<Row>[] = [
+    {
+      header: 'Chromosome',
+      value: (row) => row.chromosome,
+    },
+    {
+      header: 'Start',
+      value: (row) => row.start.toLocaleString(),
+    },
+    {
+      header: 'Stop',
+      value: (row) => row.stop.toLocaleString(),
+    },
+    {
+      header: 'Cell Type',
+      value: (row) => row.cellType,
+    },
+    {
+      header: 'Stringency',
+      value: (row) => row.stringency,
+    },
+    {
+        header: 'Experiment Accession',
+        value: (row) => row.experimentAccession,
+    },
+  ];
+  
+
+const ProCapPeaksMockData: Row[] = [
+    { id: 1, chromosome: 'chr1', start: 100, stop: 200, cellType: 'N/A', stringency: 'N/A', experimentAccession: 'ENCFF560ALK' },
+    { id: 2, chromosome: 'chr2', start: 300, stop: 400, cellType: 'N/A', stringency: 'N/A', experimentAccession: 'ENCSR273ORE' },
+  ];
+
 export const TranscriptionAtcCREs = (props: { coordinates }) => {
-return <></>
+return <>
+  <DataTable
+      rows={ProCapPeaksMockData}
+      columns={ProCapPeaksColumns}
+      tableTitle="Pro Cap Peaks Data"
+      itemsPerPage={5}
+      searchable={true}
+    />
+
+</>
 }
