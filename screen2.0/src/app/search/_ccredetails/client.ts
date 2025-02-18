5d0caef87d3b400987df3b6c06b38a12f50e7fd4
NishiPhalke <nishaphalke@gmail.com>
2025-02-18 11:35:55 -0500

    Deployed 4acf712 with MkDocs version: 1.6.1

--- screen2.0/src/app/search/_ccredetails/client.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client"
-import Config from "../../../config.json"
-export const client = new ApolloClient({
-  uri: Config.API.CcreAPI,
-  cache: new InMemoryCache(),
-})
