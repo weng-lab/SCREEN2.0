a9c39d5a2c88c8994ddccd1ef3ad3b23abb44627
Jair Meza <jair3202@gmail.com>
2025-07-08 11:06:20 -0400

    using track-logic package

--- screen2.0/src/common/MuiXLicense.tsx
+++ screen2.0/src/common/MuiXLicense.tsx
@@ -1,5 +1,5 @@
-'use client';
-import { LicenseInfo } from '@mui/x-license';
+"use client";
+import { LicenseInfo } from "@mui/x-license";
 
 LicenseInfo.setLicenseKey(process.env.NEXT_PUBLIC_MUI_X_LICENSE_KEY);
 
