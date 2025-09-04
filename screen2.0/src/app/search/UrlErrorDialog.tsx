74bbd5d33b9c834bee7992ba752fe127e869b822
NishiPhalke <nishaphalke@gmail.com>
2025-09-04 12:36:26 -0400

    show corresponding v4 cCREs if v2 cCRE is searched

--- screen2.0/src/app/search/UrlErrorDialog.tsx
+++ screen2.0/src/app/search/UrlErrorDialog.tsx
@@ -1,21 +1,63 @@
 import { ExpandMore, Home, ReportProblem } from "@mui/icons-material"
-import { Accordion, AccordionDetails, AccordionSummary, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material"
+import { Accordion, AccordionDetails, AccordionSummary, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Typography } from "@mui/material"
 import Link from "next/link"
+import { List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 
 type UrlErrorDialogProps = {
   open: boolean
   searchParams: { [key: string]: string | undefined }
   errorMsg: string
+  
 }
 
 const UrlErrorDialog = ({errorMsg, open, searchParams}: UrlErrorDialogProps) => {
 
   return (
     <Dialog open={open}>
-      <DialogTitle>
-        Whoops, we ran into an error parsing that search!
-      </DialogTitle>
+     
+     <DialogTitle>
+  {errorMsg?.includes("V2 cCRE redirection error :") ? (
+    <>
+      {searchParams.q || searchParams.accessions}{" "}
+      <Typography
+        component="span"
+        variant="body2"
+        color="text.secondary"
+        sx={{ fontSize: "0.95rem" }}
+      >
+        ({errorMsg.split("..")[1]})
+      </Typography>{" "}
+      is a v2 cCRE
+    </>
+  ) : (
+    "Whoops, we ran into an error parsing that search!"
+  )}
+</DialogTitle>
       <DialogContent>
+
+      
+        {errorMsg?.includes("V2 cCRE redirection error :") ? errorMsg.split("..")[2]==="" ? <DialogContentText>This cCRE accession has been deprecated</DialogContentText> : <DialogContentText>
+          Please refer to following intersecting v4 cCRE(s) in assembly {searchParams.assembly}:
+          <List dense>
+            {errorMsg.split("..")[2].split(",").map((acc, idx) => (
+              <ListItem key={idx}>
+                <ListItemIcon sx={{ minWidth: "20px" }}>
+                  <FiberManualRecordIcon sx={{ fontSize: "0.6rem" }} />
+                </ListItemIcon>
+                <span>
+                  <Link
+                    target="_blank"
+                    href={`/search?q=${acc}&assembly=${searchParams.assembly}`}
+                  >
+                    {acc}
+                  </Link>{" "}
+                  ({errorMsg.split("..")[3].split(",")[idx]})
+                </span>
+              </ListItem>
+            ))}
+          </List>
+        </DialogContentText> :<>
         <DialogContentText>
           Trying to parse:
         </DialogContentText>
@@ -36,6 +78,7 @@ const UrlErrorDialog = ({errorMsg, open, searchParams}: UrlErrorDialogProps) => 
         <DialogContentText>
           If this is unexpected based on the info above, please feel free to <Link href="/about#contact-us">report it using our "Contact Us" form</Link> and provide us the error you're facing. Please copy the error before navigating! It will be lost.
         </DialogContentText>
+        </>}
       </DialogContent>
       <DialogActions sx={{justifyContent: "space-between"}}>
           <Button LinkComponent={Link} href="/about#contact-us" endIcon={<ReportProblem />} variant="outlined">
@@ -45,6 +88,8 @@ const UrlErrorDialog = ({errorMsg, open, searchParams}: UrlErrorDialogProps) => 
             Go To Home
           </Button>
       </DialogActions>
+      
+
     </Dialog>
   )
 }
